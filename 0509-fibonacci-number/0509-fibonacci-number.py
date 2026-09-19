8f748336015344645f852ab08dd2d74ae8d2ab5e
Saad <sbinzahid2@gmail.com>
2026-09-19 11:30:32 +0500

    Time: 41 ms (85.17%), Space: 19.2 MB (19.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        
         first_term, second_term = 0, 1
 
         for _ in range(n):
